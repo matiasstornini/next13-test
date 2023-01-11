361e4f9bd9464e7c337352359cbca27344f2611f
adiccfut <matias.stornini00@gmail.com>
2023-01-11 16:19:48 -0300

    add analytics -in test-

--- components/header.tsx
+++ components/header.tsx
@@ -7,61 +7,86 @@ import RefreshIcon from "@mui/icons-material/Refresh";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import Stack from "@mui/material/Stack";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Script from "next/script";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
-  color: theme.palette.text.primary
+  color: theme.palette.text.primary,
 }));
 
-
 function Header() {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Item>
-          {" "}
-          <Typography variant="h3" gutterBottom>
-            PirloTV
-          </Typography>
-          <div>
-            <Typography variant="h6" component="h6">
-              ¡SEGUINOS!
+    <>
+      <Head>
+        <title>PirloTV nextjs</title>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-00NP669MKP"
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-00NP669MKP', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </Script>
+      </Head>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Item>
+            {" "}
+            <Typography variant="h3" gutterBottom>
+              PirloTV
             </Typography>
-            <Button
-              color="primary"
-              target="_blank"
-              draggable={false}
-              href="https://www.facebook.com/innovadesignalp"
-            >
-              <FacebookIcon />
-            </Button>
-            {""}
-            <Button
-              color="secondary"
-              target="_blank"
-              draggable={false}
-              href="https://www.instagram.com/innovadesignalp/"
-            >
-              <InstagramIcon />
-            </Button>
+            <div>
+              <Typography variant="h6" component="h6">
+                ¡SEGUINOS!
+              </Typography>
+              <Button
+                color="primary"
+                target="_blank"
+                draggable={false}
+                href="https://www.facebook.com/innovadesignalp"
+              >
+                <FacebookIcon />
+              </Button>
+              {""}
+              <Button
+                color="secondary"
+                target="_blank"
+                draggable={false}
+                href="https://www.instagram.com/innovadesignalp/"
+              >
+                <InstagramIcon />
+              </Button>
+            </div>
+          </Item>
+          <p></p>
+          <div>
+            <Stack direction="row" spacing={2}>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => router.reload()}
+              >
+                <RefreshIcon />
+              </Button>
+            </Stack>
           </div>
-        </Item>
-        <p></p>
-        <div>
-          <Stack direction="row" spacing={2}>
-            <Button variant="outlined" color="primary" onClick={() => router.reload()}>
-              <RefreshIcon />
-            </Button>
-          </Stack>
-        </div>
-        <p></p>
+          <p></p>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
 export default Header;
